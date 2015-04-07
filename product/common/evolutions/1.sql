3a515da876b878d5b31b17363fef967795c2a967
vehpsr <vehpsrr@gmail.com>
2015-04-07 21:25:13 +0300

    evolutions + evolutions cleanup

--- product/common/evolutions/1.sql
+++
@@ -1,9 +0,0 @@
-create table Song (
-    id bigint not null auto_increment,
-    artist varchar(40) not null,
-    title varchar(60) not null,
-    url varchar(60) not null,
-    PRIMARY KEY (id),
-    UNIQUE (url),
-    UNIQUE (artist, title)
-);
